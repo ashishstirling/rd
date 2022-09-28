460ebc26a5e4698bbc9dd93e084d0680fcefd50f
ashishstirling <ashishkulkarni@hotmail.com>
2022-09-28 18:24:33 +0100

    Static data moved to json data files

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import {Nav, Navbar, Container} from 'react-bootstrap';
+import Websites from './data/websites.json'
 
 function MyNavbar() {
-
-    const Websites = [
-        { "href": "https://google.com/", "target": "_blank", "name": "Google"},
-        { "href": "https://amazon.co.uk/", "target": "_blank", "name": "Amazon"},
-        { "href": "https://news.bbc.co.uk/", "target": "_blank", "name": "BBC"},
-        { "href": "https://aws.amazon.com/", "target": "_blank", "name": "AWS"},
-        { "href": "https://scotsoft.scot/", "target": "_blank", "name": "ScotSoft"}
-      ];
 
     return (
     <Navbar bg="light" expand="md">  
